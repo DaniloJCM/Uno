4674e3250274df2cb883ce2e1d42b0ade818e6ff
Danilo Carrasquel <144570309+DaniloJCM@users.noreply.github.com>
2024-03-17 12:01:06 -0400

    Add files via upload

---
+++ Main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+#include "Funcoes.c"
+
+int main(){
+    limparTela();
+    srand(time(NULL)); //Concerta o bug do randômico ser sempre o mesmo
+    Baralho *baralho = pegaBaralhoNovo();
+    Descarte *descarte = criaDescarte();
+    inicializaBaralho(baralho);
+    inicializaDescarte(descarte, baralho);
+
+//    delayNaTela(5);
+    Pessoas *pessoas = criaPessoa();
+    inicializaPessoas(pessoas);
+    reparteAsCartas(baralho, pessoas);
+//    imprimePessoas(pessoas);
+
+    PessoasNo *p = pessoas->prim;
+    for(int cont = 0; cont < 5; cont++){
+        jogadaDe(descarte, p->jogador);
+        p = p->prox;
+    }
+}
